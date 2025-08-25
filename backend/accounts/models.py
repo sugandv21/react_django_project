6eaa2517f693765fe7191c0de5214ea2006d4ee0
sugandv21 <sdvsuganya@gmail.com>
2025-08-25 23:10:44 +0530

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,12 +1,11 @@
-# accounts/models.py
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    USERNAME_FIELD = "email"  # Use email to login
-    REQUIRED_FIELDS = ["username"]  # username required for registration
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.name
