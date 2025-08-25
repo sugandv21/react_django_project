28328410f9bf0b278ff0beea9aeb7611ef7a6012
sugandv21 <sdvsuganya@gmail.com>
2025-08-25 15:59:46 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,5 +1,11 @@
+// import axios from "axios";
+
+// export const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/", 
+// });
+
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", 
+  baseURL: "https://react-django-project-1-mov9.onrender.com/api/",
 });
