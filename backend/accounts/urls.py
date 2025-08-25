6307cfe871a1bbbf59f2bb4c8e06c2150f072a32
sugandv21 <sdvsuganya@gmail.com>
2025-08-25 17:02:57 +0530

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterView, LoginView
 
 urlpatterns = [
-    path("api/register/", RegisterView.as_view(), name="register"),
-    path("api/login/", LoginView.as_view(), name="login"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
