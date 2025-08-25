dc8698e3c4ac4dbfb6cc8eb17e6d0d7750556c54
sugandv21 <sdvsuganya@gmail.com>
2025-08-25 23:10:19 +0530

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,10 @@
+# contact/admin.py
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at') 
+    search_fields = ('name', 'email', 'phone')  
+    list_filter = ('created_at',)  
+    ordering = ('-created_at',) 
